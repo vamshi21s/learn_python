8e5a28a918a06415b24954456731cffe3d1718bb
vamshi21s <svamshikrishna21@gmail.com>
2026-05-26 01:22:08 +0530

    did the changes in 1_lesson

---
+++ 1_lesson.py
@@ -0,0 +1,13 @@
+# print("Hello world!")
+# print("Hello")
+# name = input("enter is your name: ")
+# print("Hello " + name)
+
+name = input("enter your name: ")
+
+if name == "vamshi":
+    print("hello "+name)
+else: 
+    print("Just Hello ")
+# endif.
+#
